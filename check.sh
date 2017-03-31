e01fa4e4f364e437b702268b01a45458188c7398
zeusintuivo <zeus@intuivo.com>
2017-03-31 17:52:22 +0200

    upgraded check.sh to do intergration and cucumber and check for phantom js

--- check.sh
+++ check.sh
@@ -10,6 +10,8 @@
 [[ -z "${RED}" ]] && RED="\\033[38;5;1m"
 [[ -z "${BRIGHT_BLUE87}" ]] && BRIGHT_BLUE87="\\033[38;5;87m"
 [[ -z "${RESET}" ]] && RESET="\\033[0m"
+[[ -z "${YELLOW220}" ]] && YELLOW220="\\033[38;5;220m"
+
 
 if [ ! -d .git ] ; then
 {
@@ -29,14 +31,14 @@ if ! command -v bundle >/dev/null 2>&1; then
   exit 1
 }
 fi
-if [ ! -d .bundle/ ] ; then
-{
-  echo " "
-  echo -e "${RED} ERROR: There is no .bundle/ directory.${RESET}"
-  echo " "
-  exit 1
-}
-fi
+#if [ ! -d .bundle/ ] ; then
+# {
+#  echo " "
+#  echo -e "${RED} ERROR: There is no .bundle/ directory.${RESET}"
+#  echo " "
+#  exit 1
+#}
+#fi
 if ! command -v rubocop >/dev/null 2>&1; then
 {
   echo " "
@@ -82,32 +84,108 @@ echo
 echo " "
 echo -e "${CYAN} Rubocop only files from this branch"
 echo -e "${PURPLE_BLUE}+-+"
-echo "  +"
+echo -e "  +"
 echo -e "  +-- ${CYAN} Locating files that changes in this branch "
 echo -e "${PURPLE_BLUE}  +${GRAY241}"
 
-FILES1=$(git diff --name-only "${BRANCH}" $(git merge-base "${BRANCH}" master) | egrep ".rb|.rake")
-FILES2=$(git status -sb | egrep -v "##" | cut -c4- | egrep -v ".csv|.sh|.yml|.gitignore|.log|.txt|.key|.crt|.csr|.idl|.json|.js|.jpg|.png|.html|.gif|.feature|.scss|.css|.haml|.erb|.otf|.svg|.ttf|.tiff|.woff|.eot|.editorconfig|.markdown|.headings")
+FILES1=$(git diff --name-only "${BRANCH}" $(git merge-base "${BRANCH}" master) | egrep "\.rb|\.rake")
+FILES2=$(git status -sb | egrep -v "##" | cut -c4- | egrep -v "\.csv|\.sh|\.yml|\.gitignore|\.log|\.txt|\.key|\.crt|\.csr|\.idl|\.json|\.js|\.jpg|\.png|\.html|\.gif|\.feature|\.scss|\.css|\.haml|\.erb|\.otf|\.svg|\.ttf|\.tiff|\.woff|\.eot|\.editorconfig|\.markdown|\.headings")
 FILES="${FILES1}
 ${FILES2}"
-echo -e "${PURPLE_BLUE}   +"
-echo -e "  +---------- ${CYAN}Rubo ${PURPLE_BLUE}- checking files ${CYAN}-----------+ "
-echo -e "${PURPLE_BLUE}   +                                            + "
-echo -e "~~+                                            +~~${GRAY241}"
-while read -r ONE_FILE; do
-{
-  if [ ! -z "${ONE_FILE}" ] ; then
-  {
-    echo "  + ${ONE_FILE}"
-  }
-  fi
-}
-done <<< "${FILES}"
 
-echo "${FILES}" | sort -n | uniq | xargs bundle exec rubocop -a
-echo -e "${PURPLE_BLUE} ~~+                                            +~~"
-echo "  +                                            + "
-echo -e "  +--------------------------------------------+ ${GRAY241}"
+FILES=$(echo "${FILES}" | sort | uniq)
+
+NUMBER_LEN="${#FILES}"
+
+if (( ${NUMBER_LEN} == 0  )) ; then
+{
+  echo -e "${PURPLE_BLUE}  + ${RED}No files found for rubocop"
+}
+else
+{
+
+  echo -e "${PURPLE_BLUE}  +"
+
+  FILE_LONGEST=0
+  FILE_LEN=0
+  while read -r ONE_FILE; do
+  {
+    if [ ! -z "${ONE_FILE}" ] ; then
+    {
+      FILE_LEN="${#ONE_FILE}"
+      if (( $FILE_LEN > $FILE_LONGEST )) ; then
+      {
+        FILE_LONGEST=$FILE_LEN
+      }
+      fi
+    }
+    fi
+  }
+  done <<< "${FILES}"
+
+  (( FILE_LONGEST++ ))
+  (( FILE_LONGEST++ ))
+  SPACER=""
+  LINER=""
+  TITLER="  +----------${CYAN}Rubocop${PURPLE_BLUE}---${CYAN}checking${PURPLE_BLUE}---${CYAN}files${PURPLE_BLUE}-"
+  COUNTER=0
+  until [ $COUNTER -ge $FILE_LONGEST ]; do
+  {
+    (( COUNTER++ ))
+    SPACER="${SPACER} "
+    LINER="${LINER}-"
+    if (( $COUNTER > 37 )) ; then
+    {
+      TITLER="${TITLER}-"
+    }
+    fi
+  }
+  done
+  TITLER="${TITLER}+"
+  echo -e "${TITLER}"
+  echo -e "${PURPLE_BLUE}  +${SPACER}+ "
+  echo -e "~~+${SPACER}+~~${GRAY241}"
+  FILERS=""
+  ALL_FILERS=""
+  while read -r ONE_FILE; do
+  {
+    if [ ! -z "${ONE_FILE}" ] ; then
+    {
+      FILE_LEN="${#ONE_FILE}"
+      FILERS=" ${ONE_FILE}"
+      COUNTER=$FILE_LEN
+      (( COUNTER++ ))
+      (( COUNTER++ ))
+      until [ $COUNTER -ge $FILE_LONGEST ]; do
+      {
+        (( COUNTER++ ))
+        FILERS="${FILERS}§"
+      }
+      done
+      ALL_FILERS="${ALL_FILERS}
+  ${FILERS}"
+      #echo -e "${PURPLE_BLUE}  + ${BRIGHT_BLUE87}${ONE_FILE} ${PURPLE_BLUE}  + "
+    }
+    fi
+  }
+  done <<< "${FILES}"
+  while read -r ONE_FILE; do
+  {
+    if [ ! -z "${ONE_FILE}" ] ; then
+    {
+      echo -e "  ${PURPLE_BLUE}+${CYAN} ${ONE_FILE} ${PURPLE_BLUE}+" | sed 's@§@ @g'
+    }
+    fi
+  }
+  done <<< "${ALL_FILERS}"
+  #echo "${FILES}" | sort -n | uniq | xargs bundle exec rubocop -a
+  echo -e "${PURPLE_BLUE}~~+${SPACER}+~~"
+  echo -e "  +${SPACER}+ "
+  }
+fi
+  echo -e "${PURPLE_BLUE}  +${LINER}+ ${GRAY241}"
+
+
 
 
 # Then get folder based on ruby version 2.2.5 and rake version 10.5.0 used for development
@@ -115,8 +193,9 @@ LOCATION_RAKE_LIB=$(locate test_loader.rb | grep "rake-10.5.0/lib" | grep "ruby-
 RAKELOADER_LIB_FOLDER=$(echo ${LOCATION_RAKE_LIB%/*})
 RAKE_LIB_FOLDER=$(echo ${RAKELOADER_LIB_FOLDER%/*})
 
+
 # ALL THE TESTS
-# ruby -I"lib:test" -I"$HOME/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib" "$HOME/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib/rake/rake_test_loader.rb" "test/models/insurance_test.rb" "test/workers/twilio_cleaner_worker_test.rb" "test/controllers/account/doctors_controller_test.rb" "test/controllers/doctors/specialties_controller_test.rb" "test/workers/dtms_cleaner_worker_test.rb" "test/controllers/accounts_controller_test.rb" "test/integration/inquiry_plugin_integration_test.rb" "test/controllers/inquiries/confirmations_controller_test.rb" "test/integration/practice_integration_test.rb" "test/services/unprocessed_bookings_test.rb" "test/mailers/user_mailer_test.rb" "test/validators/partner_token_validator_test.rb" "test/models/timeslot_test.rb" "test/lib/tasks/cleanup_email_test.rb" "test/lib/tasks/cleanup_sms_test.rb" "test/models/account_test.rb" "test/models/booking_test.rb" "test/integration/patient_flows_test.rb" "test/controllers/account_backend_controller_test.rb" "test/lib/tasks/unprocessed_bookings_reminders_test.rb" "test/models/inquiry_test.rb" "test/models/partner_test.rb" "test/mailers/smser_test.rb" "test/controllers/directory_controller_test.rb" "test/controllers/account/calendars_controller_test.rb" "test/models/patient_test.rb" "test/integration/review_integration_test.rb" "services/place_service/tests/address_serializer_test.rb"
+#ruby -I"lib:test" -I"$HOME/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib" "$HOME/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib/rake/rake_test_loader.rb" "test/models/insurance_test.rb" "test/workers/twilio_cleaner_worker_test.rb" "test/controllers/account/doctors_controller_test.rb" "test/controllers/doctors/specialties_controller_test.rb" "test/workers/dtms_cleaner_worker_test.rb" "test/controllers/accounts_controller_test.rb" "test/integration/inquiry_plugin_integration_test.rb" "test/controllers/inquiries/confirmations_controller_test.rb" "test/integration/practice_integration_test.rb" "test/services/unprocessed_bookings_test.rb" "test/mailers/user_mailer_test.rb" "test/validators/partner_token_validator_test.rb" "test/models/timeslot_test.rb" "test/lib/tasks/cleanup_email_test.rb" "test/lib/tasks/cleanup_sms_test.rb" "test/models/account_test.rb" "test/models/booking_test.rb" "test/integration/patient_flows_test.rb" "test/controllers/account_backend_controller_test.rb" "test/lib/tasks/unprocessed_bookings_reminders_test.rb" "test/models/inquiry_test.rb" "test/models/partner_test.rb" "test/mailers/smser_test.rb" "test/controllers/directory_controller_test.rb" "test/controllers/account/calendars_controller_test.rb" "test/models/patient_test.rb" "test/integration/review_integration_test.rb" "services/place_service/tests/address_serializer_test.rb"
 
 # Email
 # ruby -I"lib:test" -I"$HOME/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib" "$HOME/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib/rake/rake_test_loader.rb"  "test/lib/tasks/cleanup_email_test.rb"
@@ -125,9 +204,210 @@ RAKE_LIB_FOLDER=$(echo ${RAKELOADER_LIB_FOLDER%/*})
 
 if [ -z "${1}" ] ; then
 {
-  exit 0
+  echo -e "${PURPLE_BLUE}  + ${RED}No test was passed"
+  echo -e "${PURPLE_BLUE}  + ${CYAN}"
+  echo -e "${PURPLE_BLUE}  + ${CYAN}SAMPLE:"
+  echo -e "${PURPLE_BLUE}  + ${CYAN}        ./check.sh test/lib/tasks/cleanup_email_test.rb"
+  echo -e "${PURPLE_BLUE}  + ${CYAN}"
+  echo -e "${PURPLE_BLUE}  +${LINER}+ ${GRAY241}"
+  echo "    LOCATION_RAKE_LIB : $LOCATION_RAKE_LIB"
+  echo "RAKELOADER_LIB_FOLDER : $RAKELOADER_LIB_FOLDER"
+  echo "      RAKE_LIB_FOLDER : $RAKE_LIB_FOLDER"
+
+  # PERFORM TESTS
+  ALL_INTEGRATION_TESTS="test/models/insurance_test.rb
+test/workers/twilio_cleaner_worker_test.rb
+test/controllers/account/doctors_controller_test.rb
+test/controllers/doctors/specialties_controller_test.rb
+test/workers/dtms_cleaner_worker_test.rb
+test/controllers/accounts_controller_test.rb
+test/integration/inquiry_plugin_integration_test.rb
+test/controllers/inquiries/confirmations_controller_test.rb
+test/integration/practice_integration_test.rb
+test/services/unprocessed_bookings_test.rb
+test/mailers/user_mailer_test.rb
+test/validators/partner_token_validator_test.rb
+test/models/timeslot_test.rb
+test/lib/tasks/cleanup_email_test.rb
+test/lib/tasks/cleanup_sms_test.rb
+test/models/account_test.rb
+test/models/booking_test.rb
+test/integration/patient_flows_test.rb
+test/controllers/account_backend_controller_test.rb
+test/lib/tasks/unprocessed_bookings_reminders_test.rb
+test/models/inquiry_test.rb
+test/models/partner_test.rb
+test/mailers/smser_test.rb
+test/controllers/directory_controller_test.rb
+test/controllers/account/calendars_controller_test.rb
+test/models/patient_test.rb
+test/integration/review_integration_test.rb
+services/place_service/tests/address_serializer_test.rb"
+
+
+  INTEGRATION_TESTS_EXISTS=""
+  PHANTOM_IS_REQUIRED=0
+  PHANTOM_IS_REQUIRED_BY=""
+  INTEGRATION_TEST_FILES_NOT_FOUND=""
+  while read -r ONE_FILE; do
+  {
+    if [ ! -z "${ONE_FILE}" ] ; then
+    {
+      if [ -f "${ONE_FILE}" ] ; then
+      {
+
+        if [ -z "${INTEGRATION_TESTS_EXISTS}" ] ; then
+        {
+          INTEGRATION_TESTS_EXISTS="\"${ONE_FILE}\""
+        }
+        else
+        {
+          INTEGRATION_TESTS_EXISTS="${INTEGRATION_TESTS_EXISTS} \"${ONE_FILE}\""
+        }
+        fi
+
+        #phantomjs is required
+        if [[ ! -z $(cat "${ONE_FILE}" | grep "visit") ]] ; then
+        {
+          PHANTOM_IS_REQUIRED=1
+          PHANTOM_IS_REQUIRED_BY="${PHANTOM_IS_REQUIRED_BY}
+${PURPLE_BLUE}  + ${YELLOW220}\"${ONE_FILE}\""
+        }
+        fi
+      }
+      else
+      {
+        INTEGRATION_TEST_FILES_NOT_FOUND="${INTEGRATION_TEST_FILES_NOT_FOUND}
+${PURPLE_BLUE}  + ${YELLOW220}\"${ONE_FILE}\""
+      }
+      fi
+
+    }
+    fi
+  }
+  done <<< "${ALL_INTEGRATION_TESTS}"
+
+
+
+
+  if (( ${PHANTOM_IS_REQUIRED} == 1 )) && ! command -v phantomjs >/dev/null 2>&1; then
+  {
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + ${RED} phantomjs is required by some tests "
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + Please install of make available in your path like sudo apt-get install phantomjs -y "
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + These are the tests that require phantomjs:"
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + ${PHANTOM_IS_REQUIRED_BY}"
+    exit 130;
+  }
+  fi
+  if [[ ! -z "${INTEGRATION_TEST_FILES_NOT_FOUND}" ]] ; then
+  {
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + ${YELLOW220} WARNING  ${PURPLE_BLUE}THE FOLLOWING INTEGRATION TEST FILES WHERE NOT FOUND AND were ${YELLOW220}ignored  ${PURPLE_BLUE}from your list"
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + These are the ignored files:"
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + ${INTEGRATION_TEST_FILES_NOT_FOUND}"
+  }
+  fi
+  echo -e "${PURPLE_BLUE}  + "
+  echo -e "${PURPLE_BLUE}  + ${CYAN}TESTING NOW: ${YELLOW220} INTEGRATION"
+  echo -e "${PURPLE_BLUE}  + "
+
+  export GOOGLE_API_KEY="x1x1x1x1x1x1x1x1x1x1x1x1x1x1x1x1x1xx1x1"
+  export GOOGLE_CLIENT_ID="012345678900-gmk2gmk2gmk2gmk2gmk2gmk2gmk2gmk2.apps.googleusercontent.com"
+  export GOOGLE_CLIENT_SECRET="QuerbyQuerbyErgoIpsonHop"
+  export GOOGLE_EMAIL_DOMAIN="weise.box"
+
+  LOAD_TEMP_KEYS="# Empty script"
+  [ -f .temp_keys ] && LOAD_TEMP_KEYS=$(<.temp_keys)
+  eval "${LOAD_TEMP_KEYS}"
+
+  echo -e "${PURPLE_BLUE}  + TEMP KEYS NEED IT FOR: test/integration/inquiry_plugin_integration_test.rb"
+  echo -e "${PURPLE_BLUE}  + "
+  echo -e "${PURPLE_BLUE}  + GOOGLE_API_KEY      :${GRAY241}${GOOGLE_API_KEY}"
+  echo -e "${PURPLE_BLUE}  + GOOGLE_CLIENT_ID    :${GRAY241}${GOOGLE_CLIENT_ID}"
+  echo -e "${PURPLE_BLUE}  + GOOGLE_CLIENT_SECRET:${GRAY241}${GOOGLE_CLIENT_SECRET}"
+  echo -e "${PURPLE_BLUE}  + GOOGLE_EMAIL_DOMAIN :${GRAY241}${GOOGLE_EMAIL_DOMAIN}"
+  echo -e "${PURPLE_BLUE}  + "
+  echo -e "${PURPLE_BLUE}  + ${CYAN}ruby -I\"lib:test\" -I\"${RAKE_LIB_FOLDER}\" \"${LOCATION_RAKE_LIB}\" ${YELLOW220}${INTEGRATION_TESTS_EXISTS}${RED}"
+  echo -e "${PURPLE_BLUE}  + ${RED}"
+  eval "ruby -I\"lib:test\" -I\"${RAKE_LIB_FOLDER}\" \"${LOCATION_RAKE_LIB}\" " ${INTEGRATION_TESTS_EXISTS}
+  #ruby -I"lib:test" -I"/home/vagrant/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib" "/home/vagrant/.rvm/gems/ruby-2.2.5/gems/rake-10.5.0/lib/rake/rake_test_loader.rb" "test/models/insurance_test.rb" "test/workers/twilio_cleaner_worker_test.rb" "test/controllers/account/doctors_controller_test.rb" "test/controllers/doctors/specialties_controller_test.rb" "test/workers/dtms_cleaner_worker_test.rb" "test/controllers/accounts_controller_test.rb" "test/integration/inquiry_plugin_integration_test.rb" "test/controllers/inquiries/confirmations_controller_test.rb" "test/integration/practice_integration_test.rb" "test/services/unprocessed_bookings_test.rb" "test/mailers/user_mailer_test.rb" "test/validators/partner_token_validator_test.rb" "test/models/timeslot_test.rb" "test/models/account_test.rb" "test/models/booking_test.rb" "test/integration/patient_flows_test.rb" "test/controllers/account_backend_controller_test.rb" "test/lib/tasks/unprocessed_bookings_reminders_test.rb" "test/models/inquiry_test.rb" "test/models/partner_test.rb" "test/mailers/smser_test.rb" "test/controllers/directory_controller_test.rb" "test/controllers/account/calendars_controller_test.rb" "test/models/patient_test.rb" "test/integration/review_integration_test.rb" "services/place_service/tests/address_serializer_test.rb"
+  echo -e "${PURPLE_BLUE}  + ${RESET}"
+  echo -e "${PURPLE_BLUE}  + ${RESET}"
+
+
+
+  # PERFORM TESTS
+  ALL_CUCUMBER_TESTS="features/sms_resilience.feature
+features/admin/support_inquiry_interface.feature
+features/inquiry_dispatch.feature
+features/publishers/custom_checkout_template.feature
+features/admin/admin_booking_interface.feature
+features/admin/practice_management.feature
+features/review.feature
+features/times.feature
+features/account/inquiry_management.feature"
+
+
+
+  CUCUMBER_TESTS_EXISTS=""
+  CUCUMBER_TEST_FILES_NOT_FOUND=""
+  while read -r ONE_FILE; do
+  {
+    if [ ! -z "${ONE_FILE}" ] ; then
+    {
+      if [ -f "${ONE_FILE}" ] ; then
+      {
+
+        if [ -z "${CUCUMBER_TESTS_EXISTS}" ] ; then
+        {
+          CUCUMBER_TESTS_EXISTS="'${ONE_FILE}'"
+        }
+        else
+        {
+          CUCUMBER_TESTS_EXISTS="${CUCUMBER_TESTS_EXISTS} '${ONE_FILE}'"
+        }
+        fi
+      }
+      else
+      {
+        CUCUMBER_TEST_FILES_NOT_FOUND="${CUCUMBER_TEST_FILES_NOT_FOUND}
+${PURPLE_BLUE}  + ${YELLOW220}'${ONE_FILE}'"
+      }
+      fi
+
+    }
+    fi
+  }
+  done <<< "${ALL_CUCUMBER_TESTS}"
+
+ if [[ ! -z "${CUCUMBER_TEST_FILES_NOT_FOUND}" ]] ; then
+  {
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + ${YELLOW220} WARNING  ${PURPLE_BLUE}THE FOLLOWING CUCUMBER TEST FILES WHERE NOT FOUND AND were ${YELLOW220}ignored  ${PURPLE_BLUE}from your list"
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + These are the ignored files:"
+    echo -e "${PURPLE_BLUE}  + "
+    echo -e "${PURPLE_BLUE}  + ${CUCUMBER_TEST_FILES_NOT_FOUND}"
+  }
+  fi
+
+  echo -e "${PURPLE_BLUE}  + "
+  echo -e "${PURPLE_BLUE}  + ${CYAN}TESTING NOW: ${YELLOW220} CUCUMBER"
+  echo -e "${PURPLE_BLUE}  + "
+  echo -e "${PURPLE_BLUE}  + "
+  echo -e "${PURPLE_BLUE}  + ${CYAN}bundle exec cucumber ${YELLOW220}${CUCUMBER_TESTS_EXISTS}${RED}"
+  echo -e "${PURPLE_BLUE}  + ${RED}"
+  eval "bundle exec cucumber ${CUCUMBER_TESTS_EXISTS}"
+  echo -e "${PURPLE_BLUE}  + ${RESET}"
+  echo -e "${PURPLE_BLUE}  + ${RESET}"
 }
-else
+else # -z ${1}
 {
   echo " "
   echo "Perform Tests"
@@ -135,8 +415,7 @@ else
   # PERFORM TESTS
   ruby -I"lib:test" -I"${RAKE_LIB_FOLDER}" "${LOCATION_RAKE_LIB}" "${1}"
 
-  # SAMPLES
-  # ./check.sh test/lib/tasks/cleanup_email_test.rb
+
 }
 fi
 
